c4119537379d316bba839ec4b3647dba385b9834
Roland Clothier <77452401+roland-sinch@users.noreply.github.com>
2022-12-06 20:40:24 +0100

    day 4

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import string
+import re
 #############################
 #
 # AOC Day Four
@@ -13,16 +14,25 @@ def dataset():
         return lines
 
 assignments = dataset()
+totalMatch = 0
+totalOverlap = 0
 
 for items in assignments:
-    #print(items)
-    #split item on string
     item = list(items.split(","))
     firstPair = item[0].split("-")
     secondPair = item[1].split("-")
-    taskrangeFirst =  list(range(int(firstPair[0]), int(firstPair[1])))
-    taskrangeSecond =  list(range(int(secondPair[0]), int(secondPair[1])))
-    print(str(taskrangeFirst).strip(',')+ ' ' + str(taskrangeSecond).strip(','))
-    common = ''.join(set(str(taskrangeFirst)).intersection(str(taskrangeSecond)))
-    print(common)
-    print(common.strip("[],"))
+    taskLeft = set(range(int(firstPair[0]),int(firstPair[1])+1))
+    taskRight = set(range(int(secondPair[0]),int(secondPair[1])+1))
+    common = taskLeft.intersection(taskRight)
+    isEmpty = (len(common) == 0)
+    if isEmpty:
+        print("Set is empty")
+    else:
+        totalOverlap = totalOverlap + 1
+        print(str(taskLeft) + ' ' + str(taskRight))
+        result = taskLeft.union(taskRight)
+        if result == taskLeft or result == taskRight:
+            totalMatch = totalMatch + 1
+print('Total Match of range within range is: ' + str(totalMatch))
+print('Total Match of range overlaps is: ' + str(totalOverlap))
+
